858793ac8db1bc5426f54cbd00a1bb228cd215db
shubhamsngwn <shubham.sangwan1104@gmail.com>
2025-09-09 14:49:47 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -7,50 +7,42 @@ import { Routes, Route } from "react-router-dom";
 import Developer from "./Components/Developer";
 
 function App() {
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
 
   const toggleMode = () => {
-    if (mode === 'light') {
-      setMode('dark');
+    if (mode === "light") {
+      setMode("dark");
       showAlert("Dark mode has been enabled", "success");
-      document.body.style.backgroundColor = 'black';
+      document.body.style.backgroundColor = "black";
       console.log(mode);
     } else {
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       setTimeout(() => {
         showAlert("Light mode has been enabled", "success");
       }, 2000);
       console.log(mode);
     }
-  }
+  };
 
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
-  }
+      type: type,
+    });
+  };
 
   return (
     <>
-
-      {/* WITHOUT REACT ROUTER DOM */}
-      {/* <NavBar toggleMode={toggleMode} mode={mode} />
-      <Alert alert={alert}></Alert>
-      <div className="container my-3">
-        <TextForm mode={mode} />
-      </div> */}
-
       <NavBar toggleMode={toggleMode} mode={mode} />
       <Alert alert={alert}></Alert>
 
       <div className="container my-3">
         <Routes>
-          <Route path="/" element={<TextForm mode={mode}/>} />
-          <Route path="/home" element={<TextForm mode={mode}/>} />
+          <Route path="/" element={<TextForm mode={mode} />} />
+          <Route path="/home" element={<TextForm mode={mode} />} />
           <Route path="/developer" element={<Developer mode={mode} />} />
         </Routes>
       </div>
--- src/Components/FavColor.js
+++ src/Components/FavColor.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function FavColor() {
-    const [color, setColor] = useState("Red");
-    return (
-        <>
-            <h1>My Favourite Color is: {color}</h1>
-            <button type="button" className='red' onClick={() => setColor("Red")}>Red</button>
-            <button type="button" className='blue' onClick={() => setColor("Blue")}>Blue</button>
-            <button type="button" className='black' onClick={() => setColor("Black")}>Black</button>
-            <button type="button" className='brown' onClick={() => setColor("Brown")}>Brown</button>
-        </>
-    )
+  const [color, setColor] = useState("Red");
+  return (
+    <>
+      <h1>My Favourite Color is: {color}</h1>
+      <button type="button" className="red" onClick={() => setColor("Red")}>
+        Red
+      </button>
+      <button type="button" className="blue" onClick={() => setColor("Blue")}>
+        Blue
+      </button>
+      <button type="button" className="black" onClick={() => setColor("Black")}>
+        Black
+      </button>
+      <button type="button" className="brown" onClick={() => setColor("Brown")}>
+        Brown
+      </button>
+    </>
+  );
 }
